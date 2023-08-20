ffb841810d6aab49e664c3a94ea277e3de0ccbb1
WellingtonDefassio <well.defassio@gmail.com>
2023-08-20 02:37:54 -0300

    add endpoint to credit values in rabbitmq queues.

--- src/main/java/bcb/com/br/financial/controller/ProvisionCreditController.java
+++ src/main/java/bcb/com/br/financial/controller/ProvisionCreditController.java
@@ -1,6 +1,8 @@
 package bcb.com.br.financial.controller;
 
 import bcb.com.br.financial.controller.dto.CreatProvisionCreditRequest;
+import bcb.com.br.financial.service.CreditProvisionSendService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +13,14 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/credit")
+@RequiredArgsConstructor
 public class ProvisionCreditController {
+
+    private final CreditProvisionSendService creditProvisionSendService;
 
     @PostMapping("/send")
     public ResponseEntity<Void> create(@RequestBody @Valid CreatProvisionCreditRequest request) {
-
-
+        creditProvisionSendService.sendProvisionToQueue(request);
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/bcb/com/br/financial/controller/dto/CreatProvisionCreditRequest.java
+++ src/main/java/bcb/com/br/financial/controller/dto/CreatProvisionCreditRequest.java
@@ -2,8 +2,13 @@ package bcb.com.br.financial.controller.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class CreatProvisionCreditRequest {
+    @NotEmpty
     private String cnpj;
+    @Min(1)
     private Double value;
 }
